b265345bd5a283226abe5d32cfa3bda98187cc3c
composedemo <tomgrim24@gmail.com>
2026-06-24 23:36:21 +0700

    Add PointerInputDrag with drag gestures for moving object

--- app/src/main/java/com/example/gesturedemo/MainActivity.kt
+++ app/src/main/java/com/example/gesturedemo/MainActivity.kt
@@ -4,24 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import com.example.gesturedemo.ui.theme.GestureDemoTheme
+import kotlin.math.roundToInt
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,39 +40,28 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
-    TapPressDemo(modifier)
+    PointerInputDrag(modifier)
 }
 
 @Composable
-fun TapPressDemo(modifier: Modifier = Modifier) {
-    var textState by remember { mutableStateOf("Ожидание....") }
+fun PointerInputDrag(modifier: Modifier = Modifier) {
+    Box(modifier = modifier.fillMaxSize()) {
+        var xOffset by remember { mutableStateOf(0f) }
+        var yOffset by remember { mutableStateOf(0f) }
 
-    val tapHandler = { status: String ->
-        textState = status
-    }
-
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = modifier.fillMaxSize()
-    ) {
         Box(
             modifier = Modifier
-                .padding(10.dp)
+                .offset {
+                    IntOffset(xOffset.roundToInt(), yOffset.roundToInt())
+                }
                 .background(Color.Blue)
                 .size(100.dp)
                 .pointerInput(Unit) {
-                    detectTapGestures(
-                        onPress = { tapHandler("onPress Detected") },
-                        onDoubleTap = { tapHandler("onDoubleTap Detected") },
-                        onLongPress = { tapHandler("onLongPress Detected") },
-                        onTap = { tapHandler("onTap Detected") }
-                    )
+                    detectDragGestures { _, distance ->
+                        xOffset += distance.x
+                        yOffset += distance.y
+                    }
                 }
-        )
-
-        Text(
-            text = textState,
-            modifier = Modifier.padding(16.dp)
         )
     }
 }
